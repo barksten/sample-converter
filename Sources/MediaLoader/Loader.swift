abfba4208e93d099feb44a4ef9f942cfb2af2cea
Anders Östlin <anders.ostlin@lm.se>
2021-02-25 14:28:36 +0100

    Lägger till MediaLoader-modul och ArgumentParser

---
+++ Sources/MediaLoader/Loader.swift
@@ -0,0 +1,9 @@
+
+import Foundation
+
+public struct Loader {
+    public init() {}
+    public func load(_ file: String) {
+        print(file)
+    }
+}
--- Sources/sample-converter/main.swift
+++ Sources/sample-converter/main.swift
@@ -1,1 +1,18 @@
-print("Hello, world!")
+import ArgumentParser
+import MediaLoader
+
+struct SampleConverter: ParsableCommand {
+    @Flag(name: .shortAndLong, help: "Verbose.")
+    var verbose = false
+
+    @Argument(help: "File.")
+    var file: String
+    
+    mutating func run() throws {
+        let loader = Loader()
+        loader.load(file)
+    }
+}
+
+SampleConverter.main()
+
