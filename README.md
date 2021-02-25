c1a315db7c9967b18d08a089fd9dbb0218b62c20
Anders Östlin <anders.ostlin@lm.se>
2021-02-25 14:11:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sample-converter
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import sample_converterTests
+
+var tests = [XCTestCaseEntry]()
+tests += sample_converterTests.allTests()
+XCTMain(tests)
